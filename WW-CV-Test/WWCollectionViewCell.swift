a5ca4b82a78b2105f56b96513aec1e296e688d0f
Ed Katzman <ed@edkatzman.com>
2020-04-25 14:37:22 -0400

    CollectionVC 6x6 layout

---
+++ WW-CV-Test/WWCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  WW-CV-Test
+//
+//  Created by Ed Katzman on 4/24/20.
+//  Copyright © 2020 TDG. All rights reserved.
+//
+
+import UIKit
+
+class WWCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellLabel: UILabel!
+}
